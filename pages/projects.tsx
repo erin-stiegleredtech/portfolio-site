f4960e5ec58a0233dd07fc2682720504bb21946d
erinwoodard8 <erinwoodard8@gmail.com>
2023-07-11 00:07:18 -0400

    render ProjectLists in projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,9 +1,14 @@
 import ProjectsList from '../src/components/ProjectsList';
+import Card from "../src/components/Card";
 import { Box } from "@mui/material";
 
 
+const title = "Lorem Ipusum";
+const description =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur, sapien eu malesuada porttitor, purus sapien consequat lectus, sit amet ultricies purus purus eget nisi. Vivamus non blandit nisl. Duis nec cursus leo. Etiam tincidunt purus sed elementum ultricies. Nullam maximus justo eu ipsum volutpat, in volutpat lectus sagittis. Donec mattis nulla id nisl convallis, in pulvinar turpis ultricies. Donec venenatis ultrices euismod.";
 
 const Projects = () => {
+    return(
     <Box
     sx={{
       display: "flex",
@@ -13,8 +18,9 @@ const Projects = () => {
       gap: 4,
     }}
   >
-<ProjectsList></ProjectsList>
+<ProjectsList />
 </Box>
+)
 }
 
 export default Projects;
--- src/components/ProjectsList.tsx
+++ src/components/ProjectsList.tsx
@@ -1,22 +1,24 @@
 import Card from "./Card";
 import { FC } from "react";
+import { Typography } from "@mui/material";
 
 
-const title = "Lorem Ipusum";
+
+const ProjectsList: FC = () => {
+  const title = "Lorem Ipusum";
 const description =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur, sapien eu malesuada porttitor, purus sapien consequat lectus, sit amet ultricies purus purus eget nisi. Vivamus non blandit nisl. Duis nec cursus leo. Etiam tincidunt purus sed elementum ultricies. Nullam maximus justo eu ipsum volutpat, in volutpat lectus sagittis. Donec mattis nulla id nisl convallis, in pulvinar turpis ultricies. Donec venenatis ultrices euismod.";
 
-const ProjectsList: FC = () => {
     return(
-    <>
-<Card text={{ title: title, description: description }}></Card>
-<Card text={{ title: title, description: description }}></Card>
-<Card text={{ title: title, description: description }}></Card>
-<Card text={{ title: title, description: description }}></Card>
-<Card text={{ title: title, description: description }}></Card>
-<Card text={{ title: title, description: description }}></Card>
-<Card text={{ title: title, description: description }}></Card>
-</>)
+<>    
+<Card text={{ title: title, description: description }} />
+<Card text={{ title: title, description: description }} />
+<Card text={{ title: title, description: description }} />
+<Card text={{ title: title, description: description }} />
+<Card text={{ title: title, description: description }} />
+</>
+
+)
 }
 
 export default ProjectsList;
